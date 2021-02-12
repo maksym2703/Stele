efecc72fda8037eaa66e8086b860f6eb81284b9c
Maksym <maksim.bombel@gmail.com>
2021-02-12 09:39:03 +0200

    version_1

---
+++ gulp_Stele/#src/js/script.js
@@ -0,0 +1,46 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+
+const tabLinks = document.querySelectorAll(".tabs__item_link");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+ 
+for(let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+     
+    document.querySelector('.tabs__item.active').classList.remove("active");
+    document.querySelector('.tabs-panel.active').classList.remove("active");
+ 
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+     
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+  });
+}
+
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+});
+
+
