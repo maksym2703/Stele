06d10eeef9ea6587521ec273478741d322be7f73
Maksym <maksim.bombel@gmail.com>
2021-02-14 02:51:34 +0200

    change1

--- gulp_Stele/gulp_Stele/js/script.js
+++ gulp_Stele/gulp_Stele/js/script.js
@@ -39,7 +39,24 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true,
+              arrows: false
+            }
+          }
+          ]
     });
 });
 
